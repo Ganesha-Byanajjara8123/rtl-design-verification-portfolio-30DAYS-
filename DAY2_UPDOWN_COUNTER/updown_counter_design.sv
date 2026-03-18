bf07ebceb69a6db43f7a64119fc7529abe8cb22e
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-03-18 21:55:07 +0530

    Update updown_counter_design.sv

--- DAY2_UPDOWN_COUNTER/updown_counter_design.sv
+++ DAY2_UPDOWN_COUNTER/updown_counter_design.sv
@@ -1,1 +1,29 @@
 
+module updown_counter_8bit(
+    input clk,
+    input reset,
+    input en, 
+    input up_down, 
+    output reg [7:0] count
+);
+
+always @(posedge clk or posedge reset) begin
+  always @(posedge clk)begin
+    if (reset == 1'b1) begin
+
+        count <= 8'd0;
+    end
+    else begin
+        if (up_down == 1'b1) begin
+      if (en) begin
+        if (up_down) begin
+
+            count <= count + 1'b1;
+        end
+@@ -22,6 +24,6 @@ always @(posedge clk or posedge reset) begin
+        end
+    end
+end
+end
+
+endmodule
