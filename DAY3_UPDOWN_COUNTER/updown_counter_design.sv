b1e9b696795ceb901664c9ec25421aaf5d7f0744
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-03-18 21:48:05 +0530

    Update updown_counter_design.sv

--- DAY3_UPDOWN_COUNTER/updown_counter_design.sv
+++ DAY3_UPDOWN_COUNTER/updown_counter_design.sv
@@ -1,1 +1,33 @@
 
+// Code your design here
+// Code your design here
+module updown_counter #(parameter WIDTH = 8)(
+    input logic clk,
+    input logic reset,
+    input logic en, 
+    input logic up_down,
+    output logic [WIDTH-1:0] count,
+    output logic tc       //tc (terminal count)
+);
+
+  always_ff @(posedge clk)begin
+    if (reset == 1'b1) begin
+      
+        count <= 8'd0;
+    end
+    else begin
+      if (en) begin
+        if (up_down) begin
+            
+            count <= count + 1'b1;
+        end
+        else begin
+           
+            count <= count - 1'b1;
+        end
+    end
+end
+end
+    assign tc = (count == 8'd15);
+
+endmodule
