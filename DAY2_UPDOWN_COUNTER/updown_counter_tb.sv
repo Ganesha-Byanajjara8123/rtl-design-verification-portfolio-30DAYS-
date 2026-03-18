eb4de270a75a27af56b95c11a2520bff6d77fc5d
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-03-18 21:57:24 +0530

    Update updown_counter_tb.sv

--- DAY2_UPDOWN_COUNTER/updown_counter_tb.sv
+++ DAY2_UPDOWN_COUNTER/updown_counter_tb.sv
@@ -1,1 +1,44 @@
 
+module tb_updown_counter();
+    reg clk;
+    reg reset;
+    reg en;
+    reg up_down;
+    wire [7:0] count;
+
+    updown_counter_8bit uut (
+        .clk(clk),
+        .reset(reset),
+        .en(en),
+        .up_down(up_down),
+        .count(count)
+    );
+
+    initial begin
+        reset = 1'b1; 
+        reset = 1'b1;
+        en = 1'b0;
+        up_down = 1'b0;
+
+        #10;
+        reset = 1'b0; 
+        
+        en = 1'b1;
+        up_down = 1'b1;
+        #200; 
+        up_down = 1'b0;
+       #50;
+        reset = 1'b1;
+        #10;
+        en = 1'b0;
+        reset = 1'b0; 
+        #50;
+
+        $display("Simulation finished");
+        $finish; 
+    end
+
+    initial begin
+      $monitor("Time=%0t, Reset=%0b, en=%0b, Up/Down=%0b, Count=%0d", $time, reset, en, up_down, count);  
+    end
+endmodule
