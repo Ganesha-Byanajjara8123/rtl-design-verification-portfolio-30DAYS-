31489f569549f12dc3994a2800ea72a649462c89
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-03-24 21:02:37 +0530

    Create monitor.sv

---
+++ DAY28_alu_verification_project/Test_bench/monitor.sv
@@ -0,0 +1,31 @@
+class monitor;
+
+  mailbox mbx;
+  alu_txn txn;
+  virtual alu_if vif;
+
+  function new(mailbox mbx, virtual alu_if vif);
+    this.mbx = mbx;
+    this.vif = vif;
+  endfunction
+
+  task run();
+
+    forever begin
+
+      @(posedge vif.clk);
+
+      txn = new();
+
+      txn.A = vif.A;
+      txn.B = vif.B;
+      txn.op = vif.op;
+      txn.result = vif.result;
+
+      mbx.put(txn);
+
+    end
+
+  endtask
+
+endclass
