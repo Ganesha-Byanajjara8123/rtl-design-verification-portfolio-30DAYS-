e8f3a3b9812a1eb67cb88454824923ef07d685ef
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-03-24 21:05:42 +0530

    Create tb.sv

---
+++ DAY28_alu_verification_project/Test_bench/tb.sv
@@ -0,0 +1,30 @@
+module tb;
+
+  alu_if vif();
+  env e;
+
+  alu dut (
+    .clk(vif.clk),
+    .A(vif.A),
+    .B(vif.B),
+    .op(vif.op),
+    .result(vif.result)
+  );
+
+  initial begin
+    vif.clk = 0;
+    forever #5 vif.clk = ~vif.clk;
+  end
+
+  initial begin
+    vif.rst = 1;
+    #20;
+    vif.rst = 0;
+  end
+
+  initial begin
+    e = new(vif);
+    e.run();
+  end
+
+endmodule
