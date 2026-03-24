a9458b0c208042b9f922a7e7b71ebab4c4cf0a5e
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-03-24 20:55:27 +0530

    Create alu_if.sv

---
+++ DAY28_alu_verification_project/interface/alu_if.sv
@@ -0,0 +1,24 @@
+interface alu_if;
+
+  logic clk;
+  logic rst;
+  logic [7:0] A;
+  logic [7:0] B;
+  logic [2:0] op;
+  logic [7:0] result;
+
+  // ADD assertion
+  property add_check;
+    @(posedge clk)
+    (op == 3'b000) |-> (result == A + B);
+  endproperty
+  assert property(add_check);
+
+  // SUB assertion
+  property sub_check;
+    @(posedge clk)
+    (op == 3'b001) |-> (result == A - B);
+  endproperty
+  assert property(sub_check);
+
+endinterface
